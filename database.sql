2ae0475ec65a303bf5989781b6abd394854d8860
JD Ghuman <jdsghuman@gmail.com>
2018-11-23 13:11:57 -0600

    Add basic UI components

--- database.sql
+++ database.sql
@@ -2,6 +2,10 @@ CREATE DATABASE "weekend-to-do-app";
 
 CREATE TABLE "todolist" (
 	"id" SERIAL PRIMARY KEY,
-  "item" VARCHAR(300) NOT NULL,
+  "task_title" VARCHAR(50) NOT NULL,
+  "task_detail" VARCHAR(200) NOT NULL,
 	 "completed" BOOLEAN NOT NULL,
 );
+
+INSERT INTO todolist ("item", "completed") 
+VALUES ('Finish project', 'TRUE');
---
+++ server/public/scripts/client.js
@@ -0,0 +1,75 @@
+$(document).ready(readyNow);
+
+function readyNow() {
+  loadAddTask();
+  $('#card__container').on('click', '.fa-plus-circle', createTask);
+  $('#card__container').on('click', '#btn--cancel', handleCancelClick);
+  $('#card__container').on('click', '#btn--save', handleSaveClick);
+}
+
+let showAddTaskCard = false;
+
+// Add button component
+let addTaskCard = `
+<div class="card" style="width: 18rem;">
+  <div class="card-body card-body--add">
+    <i class="fas fa-plus-circle"></i>
+    <p class="task__add-text">Add Task</p>
+  </div>
+</div>`;
+
+// Task component
+let task = `
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`;
+
+  // New Task input component
+  let newTaskInput = `
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Add New Task</h5>
+      <input class="form-control" type="text" maxlength="50" placeholder="Task title">
+      <textarea class="form-control" class="card-text" maxlength="200" type="text" placeholder="Task Detail"></textarea>
+      <a href="#" id="btn--cancel" class="btn btn-danger">Cancel</a>
+      <a href="#" id="btn--save" class="btn btn-success">Save Task</a>
+    </div>
+  </div>`;
+
+function createTask() {
+  console.log('add button clicked');
+  $('#card__container').empty();
+  // Show New Task Card
+  $('#card__container').prepend(newTaskInput);
+
+}
+
+function getTasks() {
+  console.log('getting tasks...');
+
+  //////// WIP /////////////////
+  $('#card__container').append(task);
+  $('#card__container').append(task);
+
+}
+
+function handleCancelClick() {
+  console.log('cancel clicked');
+  $('#card__container').empty();
+  // Show Add Task button
+  $('#card__container').prepend(addTaskCard);
+  // Get tasks
+  getTasks();
+}
+
+function handleSaveClick() {
+  console.log('save clicked');
+}
+
+function loadAddTask() {
+  $('#card__container').prepend(addTaskCard);
+}
