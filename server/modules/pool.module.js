a50f2ed436283da7ece08e150d865b2e9afc2e25
JD Ghuman <jdsghuman@gmail.com>
2018-11-26 22:38:26 -0600

    Update pool module

--- server/modules/pool.module.js
+++ server/modules/pool.module.js
@@ -1,22 +1,46 @@
 const pg = require('pg');
+const url = require('url');
 
-// DB Connection
-const Pool = pg.Pool;
+let config = {};
 
-const pool = new Pool({
-  database: 'weekend-to-do-app', // name of database
-  host: 'localhost',
-  port: 5432,
-  max: 10,
-  idleTimeoutMillis: 10000 // connection timeout
-});
+if (process.env.DATABASE_URL) {
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
 
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true, // heroku requires ssl to be true
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+} else {
+  config = {
+    host: 'localhost', // Server hosting the postgres database
+    port: 5432, // env var: PGPORT
+    database: 'weekend_to_do_app', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+}
+
+// this creates the pool that will be shared by all other modules
+const pool = new pg.Pool(config);
+
+// the pool will log when it connects to the database
 pool.on('connect', () => {
-  console.log('postgresql connected');
+  console.log('Postgesql connected');
 });
 
-pool.on('error', (error) => {
-  console.log('Error connecting to DB', error);
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err) => {
+  console.log('Unexpected error on idle client', err);
 });
 
 module.exports = pool;
